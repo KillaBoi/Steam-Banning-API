50228205458d4dfa9e3b2e04d5b7f343e39aa8db
Jaydip <46409262+KillaBoi@users.noreply.github.com>
2019-07-13 18:53:31 +0100

    Added reporting functionality

--- SteamGameBanAPI.py
+++ SteamGameBanAPI.py
@@ -1,11 +1,44 @@
 import requests
 import json # Will be used to check JSON API output after implementation
+import os # Used to clean up after ourselves
 
 print("Welcome to the Steam Game Ban API!\n\nPlease select one of the options below that you would like to execute.")
 getRequest = input("\n1: Report Profile\n2: Ban Profile\n3: Unban Profile\nUse '#' to go back to this menu at any time.\n> ")
-if getRequest == "1":
-    print("You have selected to Report a Profile.")
-if getRequest == "2":
+if getRequest == "1": # Reporting Profiles
+    print("\nYou have selected to Report a Profile.")
+    print("You will require the following information. Please ensure you are are ready with it.\n• Steamworks Web API PUBLISHER Key (Normal WEBAPI Keys WILL NOT work)\n• SteamID64 of the user who was banned\n• AppID of the game where the ban was applied\n")
+    steamworksAPIkeyReporting = input("Please enter your Steamworks API PUBLISHER key\n> ")
+    steamID64Reporting = input("Please input the SteamID64 of the person you would like to report\n> ")
+    appIDReporting = input("Please input the AppID of the game where the user is being reported to\n> ")
+    
+    reportURL = 'https://partner.steam-api.com/ICheatReportingService/ReportPlayerCheating/v1/' #Make sure to update this if the API URL ever changes
+    params = {'key': steamworksAPIkeyReporting, 'steamid': steamID64Reporting, 'appid': appIDReporting}
+    
+    sendReport = requests.post(url=reportURL, data=params)
+    if sendReport.status_code == requests.codes.ok:
+        print("Your report request was successful!")
+        convertReportData = sendReport.json()
+        tested = json.dumps(convertReportData)
+
+        with open ('reportdata.txt', 'w+') as reportdata:
+            print('{"payload":['+ tested +']}', file=reportdata)
+            reportdata.close()
+        
+        with open ('reportdata.txt', 'r') as readreportdata:
+            readreportjsondata = json.load(readreportdata)
+            readreportdata.close()
+        
+        reportuser = readreportjsondata['payload'][0]['response']['reportid']
+        print('The Report ID for this session is:', reportuser)
+        os.remove("reportdata.txt")
+
+    else:
+        print("There was an error in your request.")
+
+
+
+
+if getRequest == "2": # Banning Profiles
     print("\nYou have selected to Ban a Profile.\nPlease make sure you have already reported this profile beforehand otherwise this API call will not work.\n")
     confirmReport = input("\nCan you confirm you have already reported the profile you would like to ban? Y/N\n> ")
     if confirmReport == "Y":
@@ -28,11 +61,12 @@ if getRequest == "2":
         else:
             print("There was an error in your request.")
     if confirmReport == "N":
-        print("You have not yet reported the user! This information provided from this step is needed. Program will now restart so you can complete the first step and obtain a ReportID.")
+        print("You have not yet reported the user! The information provided from reporting a user is required. Program will now close so you can complete the first step and obtain a ReportID.")
     
-if getRequest =="3":
+if getRequest =="3": # Unbanning Profiles
     print("\nYou have selected to unban a previously banned profile.")
     print("You will require the following information. Please ensure you are are ready with it.\n• Steamworks Web API PUBLISHER Key (Normal WEBAPI Keys WILL NOT work)\n• SteamID64 of the user who was banned\n• AppID of the game where the ban was applied\n")
+    print("")
     steamworksAPIkeyBanned = input("Please enter your Steamworks API PUBLISHER key\n> ")
     steamID64Banned = input("Please input the SteamID64 of the person who was banned\n> ")
     appIDBanned = input("Please input the AppID of the game where the user was banned\n> ")
@@ -47,6 +81,3 @@ if getRequest =="3":
         print("Your unban request was successful!")
     else:
         print("There was an error in your request.")
-
-else:
-    print("The option you selected do not match any of those above. Please retry.")
